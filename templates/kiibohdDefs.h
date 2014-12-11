993c913437968c18264fc985ba2664aae991781f
Jacob Alexander <haata@kiibohd.com>
2014-12-10 22:21:51 -0800

    Forgot to commit template file.

---
+++ templates/kiibohdDefs.h
@@ -0,0 +1,34 @@
+/* Copyright (C) 2014 by Jacob Alexander
+ *
+ * This file is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this file.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+<|Information|>
+
+
+#ifndef __kll_defs_h
+#define __kll_defs_h
+
+// ----- KLL Variables -----
+<|VariableInformation|>
+
+
+
+// ----- Defines -----
+<|Defines|>
+
+
+
+#endif // __kll_defs_h
+
