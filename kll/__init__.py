5ccfd20a28bc73adf40fe3fac982ea872f4d4c92
Jacob Alexander <haata@kiibohd.com>
2019-04-03 22:56:42 -0700

    Incrementing to v0.5.7.12

--- kll/__init__.py
+++ kll/__init__.py
@@ -31,7 +31,7 @@ import kll.common.stage as stage
 
 ## Variables
 
-__version__ = '0.5.7.11'
+__version__ = '0.5.7.12'
 kll_name = 'kll'
 
 # This is updated whenever there is a KLL language breaking behaviour
