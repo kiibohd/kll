6d0b32b22566a56e1ea25a849de65428d9949efd
Jacob Alexander <haata@kiibohd.com>
2020-03-07 15:07:15 -0800

    Adding flit entrypoint for kll

--- kll/__init__.py
+++ kll/__init__.py
@@ -216,6 +216,13 @@ def main(args):
     sys.exit(0)
 
 
+def wrapper():
+    '''
+    flit entrypoint for kll
+    '''
+    main(sys.argv[1:])
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
 
